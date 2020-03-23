f5a9a138fd62c82ca8f4a1e0f7c388fe0b66b697
Lachezar Enev <don_malkiq@abv.bg>
2020-03-23 23:24:14 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# pet-shop
+PetShop is a specialized web store where every registered user can publish an announcement for pets (for sale or for adoption).
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ pet-shop/src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="d-md-flex flex-md-equal">
+        <div style=" padding-left: 7%; padding-top:3%;" class="col-md-6 mt-3">
+          <h1 class="featurette-heading">Here you will find the pet you've <span style="color:peru;">always dreamed
+              about!</span></h1>
+          <p class="lead mt-4"><i class="fas fa-paw"></i> Join our family.</p>
+          <p style="padding-left:5%" class="lead"><i class="fas fa-paw" style="color: peru;"></i> If you already have an account click on <a routerLink="/auth/login"><span style="color:peru"><b>Login</b></span></a></p>
+          <p style="padding-left:10%" class="lead"><i class="fas fa-paw"></i> If you don't have an account already then click on <a routerLink="/auth/register"><span style="color:peru"><b>Register</b></span></a></p>
+          <p class="lead" style="padding-left:15%"><i class="fas fa-paw" style="color: peru;"></i> Don't wait! Go find your petMate!</p>
+        </div>
+        <div class="col-md-6 mt-5">
+          <img class="home-picture" src="../assets/pets.png" />
+        </div>
+      </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.home-picture{
+    top:0;
+    left:0;
+    transform: none;
+    width: 100%;
+    position: static;
+    margin: 0 auto;
+  }
+</style>
