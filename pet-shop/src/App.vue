eaf36721d08fd701c60d8a64e7cbe3db927404ae
Lachezar Enev <don_malkiq@abv.bg>
2020-03-24 19:27:23 +0200

    not logged in homePage

--- pet-shop/src/App.vue
+++ pet-shop/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
+  <div class="image">
     <app-home />    
   </div>
+</div>
 </template>
 
 <script>
@@ -19,9 +21,14 @@ export default {
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  -moz-osx-font-smoothing: grayscale;  
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.image{
+    height:auto; min-height: 40em; background-size:cover; width:auto;
+    background-image:url('./assets/background.jpg');
+    background-position:50% 50%;
+  }
 </style>
--- pet-shop/src/components/Home.vue
+++ pet-shop/src/components/Home.vue
@@ -1,12 +1,11 @@
 <template>
 <div class="d-md-flex flex-md-equal">
-        <div style=" padding-left: 7%; padding-top:3%;" class="col-md-6 mt-3">
-          <h1 class="featurette-heading">Here you will find the pet you've <span style="color:peru;">always dreamed
-              about!</span></h1>
+        <div style="padding-left:7%; padding-top:3%;" class="col-md-6 mt-3">
+          <h1 class="featurette-heading">Here you will find the pet you've <span style="color:peru;">always dreamed about!</span></h1>
           <p class="lead mt-4"><i class="fas fa-paw"></i> Join our family.</p>
           <p style="padding-left:5%" class="lead"><i class="fas fa-paw" style="color: peru;"></i> If you already have an account click on <a routerLink="/auth/login"><span style="color:peru"><b>Login</b></span></a></p>
           <p style="padding-left:10%" class="lead"><i class="fas fa-paw"></i> If you don't have an account already then click on <a routerLink="/auth/register"><span style="color:peru"><b>Register</b></span></a></p>
-          <p class="lead" style="padding-left:15%"><i class="fas fa-paw" style="color: peru;"></i> Don't wait! Go find your petMate!</p>
+          <p style="padding-left:15%" class="lead" ><i class="fas fa-paw" style="color: peru;"></i> Don't wait! Go find your petMate!</p>
         </div>
         <div class="col-md-6 mt-5">
           <img class="home-picture" src="../assets/pets.png" />
